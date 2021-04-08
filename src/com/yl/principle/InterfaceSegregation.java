0093c539f16e74bf13bf488a8cc84a4657563637
cui250384093 <cui250384093@163.com>
2021-04-08 10:50:22 +0800

    feat: principles

---
+++ src/com/yl/principle/InterfaceSegregation.java
@@ -0,0 +1,11 @@
+package com.yl.principle;
+
+/**
+ * @author candk
+ * @Description 接口隔离原则
+ * 1. 客户端不应该依赖它不需要的接口，即一个类对另一个类的依赖应该建立在最小的接口上
+ * 2.
+ * @date 4/8/21 - 9:52 AM
+ */
+public class InterfaceSegregation {
+}
