7331de344c87599fb0272cf93cb017698782cd2f
cui250384093 <cui250384093@163.com>
2021-04-08 15:38:25 +0800

    feat: principles

---
+++ src/com/yl/principle/LiskovSubstitution.java
@@ -0,0 +1,13 @@
+package com.yl.principle;
+
+/**
+ * @author candk
+ * @Description 里氏替换原则
+ * 1. 所有引用基类的地方必须能透明的使用其子类的对象
+ * 2. 在使用继承时，子类尽量不要重写父类的方法
+ *      继承实际上让两个类耦合性增强了，在适当的情况下，可以通过聚合、组合、依赖来接近问题。
+ *
+ * @date 4/8/21 - 2:31 PM
+ */
+public class LiskovSubstitution {
+}
