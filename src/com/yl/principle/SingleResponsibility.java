ed7ed760c5b0a701f1ca62820be75eaba15402f1
cui250384093 <cui250384093@163.com>
2021-04-08 09:29:39 +0800

    feat: principles

---
+++ src/com/yl/principle/SingleResponsibility.java
@@ -0,0 +1,17 @@
+package com.yl.principle;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author candk
+ * @Description
+ * @date 4/8/21 - 9:27 AM
+ */
+public class SingleResponsibility {
+
+    @Test
+    public void test1() {
+
+    }
+
+}
---
+++ src/com/yl/principle/Vehicle.java
@@ -0,0 +1,13 @@
+package com.yl.principle;
+
+/**
+ * @author candk
+ * @Description
+ * @date 4/8/21 - 9:28 AM
+ */
+public class Vehicle {
+
+    public void run(String vehicle) {
+        System.out.println(vehicle + "is running.");
+    }
+}
