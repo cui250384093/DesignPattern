0b790ffcd45f1f71f1ca3899644713e541ad3c59
cui250384093 <cui250384093@163.com>
2021-04-09 16:25:57 +0800

    feat: dependency

---
+++ src/com/yl/principle/ClassDependence.java
@@ -0,0 +1,33 @@
+package com.yl.principle;
+
+/**
+ * @author candk
+ * @Description 类与类的关系
+ * @date 4/9/21 - 4:13 PM
+ */
+public class ClassDependence {
+
+    /**
+     * dependency - 依赖关系 - 类中使用到了对方
+     */
+
+    /**
+     * generalization - 泛化关系 - 实际是继承关系
+     */
+
+    /**
+     * implementation - 实现关系 - 是抽象的实现关系
+     */
+
+    /**
+     * Association - 关联关系 - 类与类之间的联系，关联具有导航性：即双向关系或单向关系，比如师生关系
+     */
+
+    /**
+     * Aggregation - 聚合关系 - 表示部分与整体的关系，整体与部分可以分开，聚合关系是关联关系的特例， 所以它具有关联关系的导航型和多重性
+     */
+
+    /**
+     * Composition - 组合关系 - 也是整体与部分的关系，但是整体与部分不可以分开。
+     */
+}
