971800408383dbefb39ae60fa82ccaef5c22e908
cui250384093 <cui250384093@163.com>
2021-04-12 15:21:08 +0800

    feat: singleton07

---
+++ src/com/yl/singleton/SingletonTest7.java
@@ -0,0 +1,23 @@
+package com.yl.singleton;
+
+/**
+ * @author candk
+ * @Description
+ * @date 4/12/21 - 3:11 PM
+ */
+public class SingletonTest7 {
+    public static void main(String[] args) {
+
+    }
+}
+
+/**
+ * 枚举实现单例模式
+ * 推荐使用
+ */
+enum Singleton7 {
+    /**
+     *
+     */
+    INSTANCE;
+}
