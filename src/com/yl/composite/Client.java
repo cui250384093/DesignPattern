825989d266044d651426b2597a3e4fc9f8777a2c
cui250384093 <cui250384093@163.com>
2021-04-26 08:52:04 +0800

    feat: composite

--- src/com/yl/composite/Client.java
+++ src/com/yl/composite/Client.java
@@ -6,4 +6,19 @@ package com.yl.composite;
  * @date 4/26/21 - 8:31 AM
  */
 public class Client {
+
+    public static void main(String[] args) {
+        University university = new University("HuNanUniversity", "Home University");
+
+        College elec = new College("Elec", "");
+        College mach = new College("Mach", "");
+
+        Department dep1 = new Department("Automation", "");
+        Department dep2 = new Department("Elec Automation", "");
+
+        elec.add(dep1);
+        elec.add(dep2);
+
+        university.print();
+    }
 }
