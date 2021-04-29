28921fbb5b1e36f9f9765faab696275d1256534b
cui250384093 <cui250384093@163.com>
2021-04-29 10:35:43 +0800

    feat: command pattern

---
+++ src/com/yl/command/NoCommand.java
@@ -0,0 +1,20 @@
+package com.yl.command;
+
+/**
+ * @author candk
+ * @Description
+ * @date 4/27/21 - 4:47 PM
+ */
+public class NoCommand implements Command {
+
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void undo() {
+
+    }
+}
