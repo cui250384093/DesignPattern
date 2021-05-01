033ea88f20a76fd48d9969f8c7d99e97d70fbcc3
cui250384093 <cui250384093@163.com>
2021-05-01 09:40:02 +0800

    feat: observer pattern

---
+++ src/com/yl/mediator/MediatorPattern.java
@@ -0,0 +1,13 @@
+package com.yl.mediator;
+
+/**
+ * @author candk
+ * @Description
+ * @date 5/1/21 - 9:35 AM
+ */
+public class MediatorPattern {
+    /**
+     * 中介者模式（mediator pattern），用一个中介对象来封装一系列的对象交互。
+     * 中介者使各个对象不需要显式的相互引用，从而使其耦合松散，而且可以独立地改变他们之间的交互
+     */
+}
