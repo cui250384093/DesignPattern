c11d5008635fbbfd21e96a8437acb755ea26cf5d
cui250384093 <cui250384093@163.com>
2021-05-05 08:26:31 +0800

    feat: state pattern

---
+++ src/com/yl/state/StatePattern.java
@@ -0,0 +1,13 @@
+package com.yl.state;
+
+/**
+ * @author candk
+ * @Description
+ * @date 5/5/21 - 8:23 AM
+ */
+public class StatePattern {
+    /**
+     * 状态模式（State Pattern）：它主要用来解决对象在多种状态转换时，需要对外输出不同的行为的问题，状态和行为是一一对应的，状态之间可以互相转换。
+     * 当一个对象的内在状态改变时，允许改变其行为，这个对象看起来就像是改变了其类。
+     */
+}
